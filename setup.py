9aede20b8cf8d4df30b823a74c407832eb223b7e
znrm <moradianzaniar@gmail.com>
2018-10-14 14:09:09 -0700

    Setup empty python package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='miniseer',
+    version='0.0.1',
+    description='ML Automation',
+    url='http://github.com/znrm/miniseer',
+    author='Zaniar Moradian',
+    author_email='zaniarmoradian@gmail.com',
+    license='MIT',
+    packages=['miniseer'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
